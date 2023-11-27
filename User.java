3bc5c8d45e74fdc8e5d8579d9cac74159fa9e01f
ArushSanghal <101238461+ArushSanghal@users.noreply.github.com>
2023-11-27 13:15:45 -0700

    User
    Added admin functionalities

--- User.java
+++ User.java
@@ -4,11 +4,19 @@ public class User{
     private String fullName;
     private String email;
     private Address address;
+    private int adminID;
+    private String password;
 
     public User(String fn, String em, Address ad){  
         this.fullName = fn;
         this.email = em;
         this.address = ad;
+
+    }
+
+    public User(int id, String pass){  
+        this.adminID = id;
+        this.password = pass;
 
     }
 
@@ -18,6 +26,18 @@ public class User{
 
     public String getUser() {
         return "Name: "+ this.fullName + ", Email: " + this.email + ", Address: " + this.address.toString();
+    }
+
+    public string setName(String name){
+        this.fullName = name;
+    }
+
+    public string setEmail(String em){
+        this.email = em;
+    }
+
+    public string setAddress(Address ad){
+        this.address = ad;
     }
 
 }
