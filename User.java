047bf51c6930c2a4d3ba4bb238c8f8d4ab2adf40
ArushSanghal <101238461+ArushSanghal@users.noreply.github.com>
2023-11-27 18:32:53 -0700

    User
    Added in userLogin, adminLogin, regUser functionalities and removed useless methods

--- User.java
+++ User.java
@@ -1,43 +1,68 @@
+import java.sql.ResultSet;
+import java.util.List;
 
 public class User{
 
     private String fullName;
     private String email;
     private Address address;
+    private String userPassword;
+    private String adminUsername;
     private int adminID;
-    private String password;
+    private String adminPassword;
+    private DatabaseConnection db;
 
-    public User(String fn, String em, Address ad){  
+    public User(String fn, String em, Address ad, String pass){  
         this.fullName = fn;
         this.email = em;
         this.address = ad;
+        this.userPassword = pass;
 
     }
 
-    public User(int id, String pass){  
-        this.adminID = id;
-        this.password = pass;
+    public User(String admName, String pass, int adminID){  
+        this.adminUsername = admName;
+        this.adminPassword = pass;
 
     }
 
-    public String regUser(){
-        return this.fullName + ", " + this.email + ", " + this.address.toString();
-    }
-
-    public String getUser() {
-        return "Name: "+ this.fullName + ", Email: " + this.email + ", Address: " + this.address.toString();
-    }
-
-    public string setName(String name){
-        this.fullName = name;
-    }
-
-    public string setEmail(String em){
+    public User(String em, String pass){
         this.email = em;
+        this.userPassword = pass;
     }
 
-    public string setAddress(Address ad){
-        this.address = ad;
+    public void regUser(){
+        db = new DatabaseConnection();
+        db.insertUser(fullName, email, address, userPassword);
+    }
+
+    public boolean loginUser(String inputEmail, String inputPass){
+        db = new DatabaseConnection();
+
+        List<String> emailList = db.extractUserEmails();
+        List<String> passwordList = db.extractUserPasskeys();
+
+        if (emailList.contains(inputEmail) && passwordList.contains(inputPass)){
+            System.out.println("User found");
+            return true;
+        }
+        System.out.println("User not found");
+        return false;
+    }
+
+    public boolean loginAdmin(String inputUsername, String inputPass){
+
+        db = new DatabaseConnection();
+
+        List<String> usernameList = db.extractAdminUsernames();
+        List<String> passwordList = db.extractAdminPasswords();
+        
+        if (usernameList.contains(inputUsername) && passwordList.contains(inputPass)){
+            System.out.println("User found");
+            return true;
+        }
+        System.out.println("User not found");
+        return false;
     }
 
 }
