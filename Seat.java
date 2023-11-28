4155a7a0c234a608ff4b048214c1ded9480479f4
ShayyanAsim <shayyan.asim@ucalgary.ca>
2023-11-28 15:28:23 -0700

    Updated Seat

--- Seat.java
+++ Seat.java
@@ -15,3 +15,45 @@ public class Seat{
         this.seat = newSeat;
     }
 }
+
+public class Seat{
+
+    private String seat;
+    private int seat_aval;
+
+    public Seat(String s, int seat_tot){  
+        this.seat = s;
+        if (seat_tot >0 ){
+            seat_aval = seat_tot - 1;
+        }
+        else{
+            throw new Exception("Flight is full");
+        }
+
+    }
+
+    public String getSeat() {
+        return this.seat;
+    }
+
+    public void setSeat(String newSeat) {
+        this.seat = newSeat;
+    }
+    
+    public int getSeatsAvailable(){
+        return this.seat_aval;
+    }
+
+    public void setSeatsAvailable(int newSeats){
+        this.seat_aval = newSeats;
+    }
+
+    public int seatTaken(int seats){
+        return seats -1;
+    }
+
+    public int seatRefunded(int seats){
+        return seats +1;
+    }
+    
+}
