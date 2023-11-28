da7d9731b8869d59101999933544155adb56f34e
ShayyanAsim <shayyan.asim@ucalgary.ca>
2023-11-27 23:21:42 -0700

    REMOVED AIRCRAFT

--- AirlineController.java
+++ AirlineController.java
@@ -6,21 +6,60 @@ import java.util.List;
 
 
 public class AirlineController {
+
+
+
+    private static void processResultSet(ResultSet resultSet, String resultType) {
+        try {
+            System.out.println(resultType + ":");
+            while (resultSet.next()) {
+                if (resultType.contains("Aircraft")) {
+                    System.out.println("Aircraft: " + resultSet.getString("Aircraft"));
+                } else if (resultType.contains("Airport")) {
+                    System.out.println("Origin: " + resultSet.getString("Origin") +
+                                       ", Destination: " + resultSet.getString("Destination"));
+                } else {
+                    System.out.println("Username: " + resultSet.getString("Username"));
+                    System.out.println("AdminID: " + resultSet.getInt("AdminID"));
+                }
+            }
+            System.out.println("-----------------------------");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
     public static void main(String[] args) {
-        // Get an instance of DatabaseConnection using the Singleton pattern
         DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
 
-        // Call the method to get flight details
+
         List<String> flightDetailsList = databaseConnection.getFlightDetails();
 
-        // Display the flight details
         for (String flightDetails : flightDetailsList) {
             System.out.println(flightDetails);
         }
 
 
+        ResultSet allAirportsResultSet = databaseConnection.browseAllAirports();
+        processResultSet(allAirportsResultSet, "All Airport Results");
+
+        ResultSet allAircraftsResultSet = databaseConnection.browseAllAircrafts();
+        processResultSet(allAircraftsResultSet, "All Aircraft Results");
+
+        ResultSet allUsersResultSet = databaseConnection.browseAllUsers();
+        processResultSet(allUsersResultSet, "All User Results");
+
+        ResultSet allAdminsResultSet = databaseConnection.browseAllAdmins();
+        processResultSet(allAdminsResultSet, "All Admin Results");
+
+
+
+        
+    }
+
+
 
     }
-}
+
 
 
