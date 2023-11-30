70f3cf3643a8c8de029b0cd5672154b6b04e0953
ShayyanAsim <shayyan.asim@ucalgary.ca>
2023-11-29 23:53:43 -0700

    Fixed some errors in Seat

--- Seat.java
+++ Seat.java
@@ -1,27 +1,11 @@
-
-public class Seat{
-
-    private String seat;
-
-    public Seat(){  
-
-    }
-
-    public String getSeat() {
-        return this.seat;
-    }
-
-    public void setSeat(String newSeat) {
-        this.seat = newSeat;
-    }
-}
+import java.util.List;
 
 public class Seat{
 
     private String seat;
     private int seat_aval;
 
-    public Seat(String s, int seat_tot){  
+    public Seat (String s, int seat_tot) throws Exception {  
         this.seat = s;
         if (seat_tot >0 ){
             seat_aval = seat_tot - 1;
@@ -57,7 +41,7 @@ public class Seat{
     }
 
     public boolean seatChecker(String seatIDInput){
-        db = new DatabaseConnection();
+        DatabaseConnection db = DatabaseConnection.getInstance();
 
         List<String> seatsAvail = db.extractSeatID();
 
