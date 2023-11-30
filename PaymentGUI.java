45a23586f2065266d5c8f9718c8d6b97f19f4383
dannicklucas <dannicklucas@gmail.com>
2023-11-30 01:19:15 -0700

    payment gui

--- PaymentGUI.java
+++ PaymentGUI.java
@@ -12,13 +12,16 @@ import java.util.*;
 public class PaymentGUI extends JFrame implements ActionListener, MouseListener{
     private JLabel definition;
     private int seat;
-    int availableSeats = 50;
+    private JTextField userText;
+    private JLabel userLabel;
+    private JLabel passLabel;
+    private JTextField passText;
 
         public PaymentGUI(int seat){
         super("Seat Select");
         this.seat = seat;
         setupGUI();
-        setSize(900,600);
+        setSize(600,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         setLocationRelativeTo(null);    
         addMouseListener(this);
@@ -29,8 +32,40 @@ public class PaymentGUI extends JFrame implements ActionListener, MouseListener{
         
         
         definition = new JLabel("Enter Payment Info:");
+
+        userLabel = new JLabel("Please Enter Email For Flight Detalis:");
+        userLabel.setBounds(20, 40, 260, 25);
+        this.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(260, 40, 165, 25);
+        this.add(userText);
         
-        JButton button = new JButton("Confirm");
+        passLabel = new JLabel("Enter Your Credit Card Number:");
+        passLabel.setBounds(20, 70, 260, 25);
+        this.add(passLabel);
+
+        passText = new JTextField(20);
+        passText.setBounds(260, 70, 165, 25);
+        this.add(passText);
+
+        passLabel = new JLabel("Expiration Date:");
+        passLabel.setBounds(160, 100, 150, 25);
+        this.add(passLabel);
+
+        passText = new JTextField(20);
+        passText.setBounds(260, 100, 50, 25);
+        this.add(passText);
+
+        passLabel = new JLabel("CVV:");
+        passLabel.setBounds(320, 100, 30, 25);
+        this.add(passLabel);
+
+        passText = new JTextField(20);
+        passText.setBounds(360, 100, 30, 25);
+        this.add(passText);
+        
+        JButton button = new JButton("Make Payment");
         button.addActionListener(this);
         
         JPanel headerPanel = new JPanel();
@@ -53,7 +88,9 @@ public class PaymentGUI extends JFrame implements ActionListener, MouseListener{
 }
 
     public void actionPerformed(ActionEvent event){
-
+        DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+        dbConnection.getConnection();
+        dbConnection.insertPassengers(not done);
         
     }
     
@@ -77,7 +114,7 @@ public class PaymentGUI extends JFrame implements ActionListener, MouseListener{
     public static void main(String[] args) {
         
         EventQueue.invokeLater(() -> {
-            new SeatSelectGUI("wooo").setVisible(true);        
+            new PaymentGUI(1).setVisible(true);        
         });
     }
 
